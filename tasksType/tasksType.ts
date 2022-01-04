d163f751ee0b73bba15f750ec401c88db1d221bc
Edmond Luu <eluu472@gmail.com>
2022-01-03 17:00:19 -0800

    taskType

--- tasksType/tasksType.ts
+++ tasksType/tasksType.ts
@@ -1,15 +1,28 @@
 export function tasksTypes(deadlines: number[], day: number): number[] {
-    const counts = [0, 0, 0];
-    deadlines.forEach(task => {
-        if (task <= day) {
-            counts[0]++;
-        } else if (day + 7 >= task) {
-            counts[1]++;
+    // const counts = [0, 0, 0];
+    // deadlines.forEach(task => {
+    //     if (task <= day) {
+    //         counts[0]++;
+    //     } else if (day + 7 >= task) {
+    //         counts[1]++;
+    //     } else {
+    //         counts[2]++;
+    //     }
+    // })
+    // return counts;
+
+
+    let [today, upcoming, later] = [0, 0, 0];
+    deadlines.forEach(deadline => {
+        if (deadline <= day) {
+            today++;
+        } else if (deadline >= day + 1 && deadline <= day + 7) {
+            upcoming++;
         } else {
-            counts[2]++;
+            later++;
         }
-    })
-    return counts;
+    });
+    return [today, upcoming, later];
 }
 
 console.log(tasksTypes([1, 2, 3, 4, 5], 2));
