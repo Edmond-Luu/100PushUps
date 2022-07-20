7e5c7f864832b94f1b1ce98cd1608db71a8a6a72
Edmond Luu <eluu472@gmail.com>
2022-07-20 10:11:12 -0700

    Finished firstDigits

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,14 @@
 export function firstDigit(inputString: string): string {
+    const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    const strArr = inputString.split("")
+    for (let i = 0; i < strArr.length; i++) {
+        if (digits.includes(strArr[i])){
+            return strArr[i];
+        };
+    };
 
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit('var_1__Int'));
+console.log(firstDigit('q2q-q'));
+console.log(firstDigit('0ss'));
