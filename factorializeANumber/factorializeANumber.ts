0e0a5f8b09b9a8893501885c2676eff90f39f7eb
Edmond Luu <eluu472@gmail.com>
2022-08-09 10:11:12 -0700

    extractMatrixColumn

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,9 +1,17 @@
 export function factorializeANumber(num: number): number {
-    let total: number = 1;
+    // let total: number = 1;
+    // for (let i = 1; i <= num; i++) {
+    //     total *= i;
+    // };
+    // return total;
+
+
+    let total = 1;
     for (let i = 1; i <= num; i++) {
         total *= i;
     };
     return total;
+
 };
 
 console.log(factorializeANumber(5));
