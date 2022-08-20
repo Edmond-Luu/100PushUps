4c5f6b208006dd8ba5ca6853c0050e609e3cc9f0
Edmond Luu <eluu472@gmail.com>
2022-08-20 10:11:12 -0700

    Finished fancyRide

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,9 @@
 export function fancyRide(l: number, fares: number[]): string {
+    const rides = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
+    const totalFares = fares.map(fare => fare * l);
+    const affortableFares = totalFares.filter(item => item <= 20);
+    const maxFare = Math.max(...affortableFares);
+    return rides[totalFares.indexOf(maxFare)];
+};
 
-}
-
-// console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
