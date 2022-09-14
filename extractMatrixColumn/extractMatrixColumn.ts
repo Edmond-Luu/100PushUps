b9049864720aeda3d3a4ab44cfa8fd70e3696491
Edmond Luu <eluu472@gmail.com>
2022-09-14 10:11:12 -0700

    extractMatrixColumn

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,9 +1,15 @@
 export function extractMatrixColumn(matrix: number[][], column: number): number[] {
-    const finalCol: number[] = [];
-    for (let i = 0; i < matrix.length; i++) {
-        finalCol.push(matrix[i][column]);
-    };
-    return finalCol;
+    // const finalCol: number[] = [];
+    // for (let i = 0; i < matrix.length; i++) {
+    //     finalCol.push(matrix[i][column]);
+    // };
+    // return finalCol;
+
+
+    
+    const matrixColumn: number[] = [];
+    matrix.forEach(row => matrixColumn.push(row[column]));
+    return matrixColumn;
 };
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
