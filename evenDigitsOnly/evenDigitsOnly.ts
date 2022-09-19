c39c292484fcd972e567473fab4fec5c8e67acc4
Edmond Luu <eluu472@gmail.com>
2022-09-19 10:11:12 -0700

    evenDigitsOnly

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,14 +1,18 @@
 export function evenDigitsOnly(n: number): boolean {
-    let evenCount:number = 0
-    const digits: number[] = [];
-    const splitStr = n.toString().split("");
-    splitStr.forEach(item => digits.push(parseInt(item)));
-    for (let digit of digits) {
-        if (digit % 2 === 0){
-            evenCount ++
-        }
-    };
-    return evenCount === splitStr.length;
+    // let evenCount:number = 0
+    // const digits: number[] = [];
+    // const splitStr = n.toString().split("");
+    // splitStr.forEach(item => digits.push(parseInt(item)));
+    // for (let digit of digits) {
+    //     if (digit % 2 === 0){
+    //         evenCount ++
+    //     }
+    // };
+    // return evenCount === splitStr.length;
+
+
+    const digits = n.toString().split("");
+    return digits.every(digit => parseInt(digit) % 2 === 0);
 };
 
 console.log(evenDigitsOnly(248622));
