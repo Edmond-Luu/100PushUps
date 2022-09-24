63e38f008301f0c935172c68500cfdab25291f36
Edmond Luu <eluu472@gmail.com>
2022-09-24 10:11:12 -0700

    Finished integerToStringOfFixedWidth

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,15 +1,35 @@
 export function integerToStringOfFixedWidth(number: number, width: number): string {
-    let newStr = [];
-    const strArr = number.toString().split("");
-    let strLength = strArr.length;
-    for (let i = 0; i < width; i++) {
-        while (strLength < width) {
-            strArr.unshift("0")
-            strLength = strArr.length
+    // let newStr = [];
+    // const strArr = number.toString().split("");
+    // let strLength = strArr.length;
+    // for (let i = 0; i < width; i++) {
+    //     while (strLength < width) {
+    //         strArr.unshift("0")
+    //         strLength = strArr.length
+    //     }
+    //     newStr.push(strArr[i])
+    // }
+    // return newStr.join("");
+
+
+
+    let stringifiedNum = number.toString();
+    const numberWidth = stringifiedNum.length;
+    const widthDiff = width - numberWidth;
+
+    if (numberWidth < width) {
+        let padStart = "";
+        for (let i = 0; i < widthDiff; i++) {
+            padStart = padStart.concat("0");
         }
-        newStr.push(strArr[i])
+        return padStart.concat(stringifiedNum);
+    };
+
+    if (numberWidth > width) {
+        return stringifiedNum.substring(widthDiff, stringifiedNum.length)
     }
-    return newStr.join("");
+
+    return stringifiedNum;
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
