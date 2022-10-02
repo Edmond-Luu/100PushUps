9777a280662aa2e16446d236a078d062aadea9ff
Edmond Luu <eluu472@gmail.com>
2022-10-02 12:00:00 -0700

    Finished domainType

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -11,6 +11,26 @@ export function domainType(domains: string[]): string[] {
         domainResults.push(domainTypes[domains[i].split(".").slice(-1)[0]]);
     };
     return domainResults;
-}
+
+
+    // const domainType: string[] = [];
+
+    // for (let i = 0; i < domains.length; i++) {
+    //     const domain = domains[i].split(".");
+    //     const lastDomain = domain[domain.length - 1];
+
+    //     if (lastDomain === "org") {
+    //         domainType.push("organization");
+    //     } else if (lastDomain === "com") {
+    //         domainType.push("commercial");
+    //     } else if (lastDomain === "net") {
+    //         domainType.push("network");
+    //     } else if (lastDomain === "info") {
+    //         domainType.push("information");
+    //     };
+    // };
+
+    // return domainType;
+};
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
