006d2d81123f57a346f0b964365aa6361e933d0f
Edmond Luu <eluu472@gmail.com>
2022-10-06 12:00:00 -0700

    Finished differentSymbolsNative

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,20 +1,37 @@
 export function differentSymbolsNaive(s: string): number {
-    const uniqueChars: string[] = [];
-    const sArr: string[] = s.split("");
-    sArr.forEach(letter => !uniqueChars.includes(letter) ? uniqueChars.push(letter) : "");
-    return uniqueChars.length;
+    // const uniqueChars: string[] = [];
+    // const sArr: string[] = s.split("");
+    // sArr.forEach(letter => !uniqueChars.includes(letter) ? uniqueChars.push(letter) : "");
+    // return uniqueChars.length;
 
 
-//     const uniqueChars: string[] = [];
-//     const inputChars: string[] = s.split("");
+    //     const uniqueChars: string[] = [];
+    //     const inputChars: string[] = s.split("");
 
-//     inputChars.forEach((char) => {
-//         if (!uniqueChars.includes(char)) {
-//             uniqueChars.push(char);
-//         }
-//     });
+    //     inputChars.forEach((char) => {
+    //         if (!uniqueChars.includes(char)) {
+    //             uniqueChars.push(char);
+    //         }
+    //     });
+    //     return uniqueChars.length
 
-//     return uniqueChars.length
-// }
+
+    // const uniqueChars = {};
+    // const inputChars: string[] = s.split("");
+
+    // inputChars.forEach((char) => {
+    //     uniqueChars[char] = 1;
+    // });
+
+    // return Object.keys(uniqueChars).length;
+
+
+
+    const uniqueChars = new Set();  //Sets have no duplicates (ES6 and above only).
+    const inputChars = s.split("");
+    inputChars.forEach((chars) => uniqueChars.add(chars));
+    return uniqueChars.size;
+
+}
 
 console.log(differentSymbolsNaive('cabca'));
