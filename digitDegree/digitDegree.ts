28a35d876fd80f989cefcdcc67abd10b342eea02
Edmond Luu <eluu472@gmail.com>
2022-10-07 12:00:00 -0700

    Finished digitDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,5 +1,28 @@
-export function digitDegree(n: number): number {
+import { parse } from "ts-node";
 
+export function digitDegree(n: number): number {
+    let count: number = 0;
+    let currentNumber = n;
+
+    if (n <= 9) {
+        return count;
+    } else {
+        while (true) {
+            count++;
+            currentNumber = getDigit(currentNumber);
+            if (currentNumber <= 9) {
+                break;
+            }
+        }
+
+
+    }
+    return count;
+};
+
+function getDigit(num: number) {
+    const nums = num.toString().split("").map(element => parseInt(element));
+    return nums.reduce((a: number, b: number) => a + b);
 };
 
 console.log(digitDegree(5));
