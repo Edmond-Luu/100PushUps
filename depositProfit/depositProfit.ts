776abb11fcf7eba8c1ed2362a5bc91b4df15c7e7
Edmond Luu <eluu472@gmail.com>
2022-10-12 15:12:40 -0700

    Finished depositProfit

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,10 @@
 export function depositProfit(deposit: number, rate: number, threshold: number): number {
-
+    let yearCount = 0;
+    while(deposit < threshold){
+        deposit = deposit * 1.2
+        yearCount ++;
+    }
+    return yearCount;
 }
 
-// console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170))
