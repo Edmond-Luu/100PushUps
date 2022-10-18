fea88c86244c106ccd1a39798dcb88b71779e7bd
Edmond Luu <eluu472@gmail.com>
2022-10-18 15:12:40 -0700

    crossingSum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,18 +1,28 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
-    let sum = 0;
+    // let sum = 0;
 
-    for (let i = 0; i < matrix[a].length; i++) {
-        sum += matrix[a][i];
-    };
+    // for (let i = 0; i < matrix[a].length; i++) {
+    //     sum += matrix[a][i];
+    // };
+
+    // for (let i = 0; i < matrix.length; i++) {
+    //     sum += matrix[i][b]
+    // };
+
+    // sum -= matrix[a][b]
+    // return sum;
+
+
+    const columnTotal = matrix[a].reduce((a, b) => a + b);
+    let rowTotal = 0;
 
     for (let i = 0; i < matrix.length; i++) {
-        sum += matrix[i][b]
+        rowTotal += i !== a ? matrix[i][b] : 0;
     };
 
-    sum -= matrix[a][b]
-    return sum;
+    return columnTotal + rowTotal;
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1],
+[2, 2, 2, 2],
+[3, 3, 3, 3]], 1, 3));
