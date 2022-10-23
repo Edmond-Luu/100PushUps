58dc2e42c8ea6d41bd09a485dcffd4ce36144570
Edmond Luu <eluu472@gmail.com>
2022-10-23 15:31:12 -0700

    Finished alternating sums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,13 @@
 export function alternatingSums(a: number[]): number[] {
-
+    let team1:number = 0;
+    let team2:number = 0;
+    for (let i=0; i<a.length; i+=2){
+        team1 += a[i]
+    }
+    for (let j=1; j<a.length; j+=2){
+        team2 += a[j]
+    }
+    return [team1, team2];
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
