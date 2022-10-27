215462237890476b64b03291f04f06b657ff4d41
Edmond Luu <eluu472@gmail.com>
2022-10-27 15:16:55 -0700

    Finished addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,17 @@
 export function addTwoDigits(n: any): number {
+    const first:string = n.toString()[0];
+    const second:string = n.toString()[1];
+    return parseInt(first) + parseInt(second);
 
+
+    // const nums = n.toString().split('');
+    // return nums.reduce((a: string, b: string) => {
+    //     return parseInt(a) + parseInt(b);
+    // })
+
+    // const nums = n.toString().split('');
+    // return parseInt(nums[0]) + parseInt(nums[1]);
+    
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
