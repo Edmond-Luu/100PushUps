4f228a2ea661c431a71b10a41e3107c9f33cd0dc
Edmond Luu <eluu472@gmail.com>
2022-10-30 14:35:13 -0700

    Finished areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -3,7 +3,7 @@ export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLef
     const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
     const friendsWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
     const friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
-    return yourWeakest === friendsWeakest && yourStrongest === friendsStrongest;
+    return (yourWeakest === friendsWeakest && yourStrongest === friendsStrongest);
 }
 
 console.log(areEquallyStrong(10, 15, 15, 10))
