23fbb84c6cf32ea34ce5c50cba5ccd7208ba6589
Edmond Luu <eluu472@gmail.com>
2022-10-30 14:27:13 -0700

    Finished alternating sums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -8,6 +8,17 @@ export function alternatingSums(a: number[]): number[] {
         team2 += a[j]
     }
     return [team1, team2];
+
+    let evenSum = 0;
+    let oddSum = 0; 
+    a.forEach((element, index) => {
+        if(index % 2 === 0){
+            evenSum += element;
+        } else{
+            oddSum += element;
+        }
+    });
+    return [evenSum, oddSum];
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
