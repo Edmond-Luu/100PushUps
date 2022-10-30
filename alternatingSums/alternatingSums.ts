197e0cbfde6ce76c992f0332a9e4d5ab3213caa0
Edmond Luu <eluu472@gmail.com>
2022-10-30 14:35:13 -0700

    Finished areEquallyStrong

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -9,16 +9,16 @@ export function alternatingSums(a: number[]): number[] {
     }
     return [team1, team2];
 
-    let evenSum = 0;
-    let oddSum = 0; 
-    a.forEach((element, index) => {
-        if(index % 2 === 0){
-            evenSum += element;
-        } else{
-            oddSum += element;
-        }
-    });
-    return [evenSum, oddSum];
+    // let evenSum = 0;
+    // let oddSum = 0; 
+    // a.forEach((element, index) => {
+    //     if(index % 2 === 0){
+    //         evenSum += element;
+    //     } else{
+    //         oddSum += element;
+    //     }
+    // });
+    // return [evenSum, oddSum];
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,9 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-    return yourLeft + yourRight === friendsRight + friendsLeft;
+    const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+    const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+    const friendsWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+    const friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+    return yourWeakest === friendsWeakest && yourStrongest === friendsStrongest;
 }
 
 console.log(areEquallyStrong(10, 15, 15, 10))
