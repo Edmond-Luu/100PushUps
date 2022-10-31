e97d36d773102260372c4fc9f1f91c4ede95a433
Edmond Luu <eluu472@gmail.com>
2022-10-31 10:20:14 -0700

    Finished chunkyMonkey

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,12 +1,24 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
     const endArray: [][] = [];
 
-    const array1 = arr.slice(0:size);
-    const array2 = arr.slice(size:arr.length);
+    const array1 = arr.slice(0,size);
+    const array2 = arr.slice(size,arr.length);
     endArray.push(array1)
     endArray.push(array2)
 
     return endArray;
+
+    
+
+    // const nested = [];
+    // let count: number = 0;
+
+    // while(count < arr.length){
+    //     nested.push(arr.slice(count, count += size));
+    // }
+
+    // return nested;
+
 }
 
 console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
