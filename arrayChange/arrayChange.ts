ce74efa2adba50794ced6fba323137e085b7713a
Edmond Luu <eluu472@gmail.com>
2022-10-31 10:12:18 -0700

    Finished arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,12 @@
 export function arrayChange(inputArray: number[]): number {
-
+    let shifts:number = 0;
+    for (let i=1; i < inputArray.length; i++){
+        while(inputArray[i] <= inputArray[i-1]){
+            inputArray[i] = inputArray[i] + 1;
+            shifts += 1;
+        }
+    } 
+    return shifts;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
