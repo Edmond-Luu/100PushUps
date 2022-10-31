4c9ca72ef134203fc4edfb1a14dc6a5927d035a0
Edmond Luu <eluu472@gmail.com>
2022-10-31 10:34:32 -0700

    Finished arrayChange

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,31 @@
 export function arrayConversion(inputArray: number[]): number {
+    let isOdd: boolean = true;
 
+    while (inputArray.length !== 1) {
+        if (isOdd) {
+            for (let i = 0; i < inputArray.length; i += 2) {
+                inputArray.splice(i, 1, inputArray[i] + inputArray[i + 1]);
+            }
+
+            for (let i = 1; i < inputArray.length; i++) {
+                inputArray.splice(i, 1);
+            }
+
+            isOdd = false;
+        }
+        else {
+            for (let i = 0; i < inputArray.length; i += 2) {
+                inputArray.splice(i ,1, inputArray[i] * inputArray[i + 1]);
+            }
+
+            for (let i = 1; i < inputArray.length; i++) {
+                inputArray.splice(i, 1);
+            }
+            isOdd = true;
+        }
+    }
+    return inputArray[0];
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
