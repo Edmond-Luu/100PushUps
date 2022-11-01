19e050659089c9552584a40df5f147f5c2ce43da
Edmond Luu <eluu472@gmail.com>
2022-11-01 10:03:11 -0700

    Finished arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,14 @@
 export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+    const newArray: number[] = [];
 
+    for (let i = 0; i < inputArray.length; i++){
+        if (inputArray[i] === elemToReplace){
+            newArray.push(substitutionElem)
+        } else{
+            newArray.push(inputArray[i]);
+        }
+    }
+    return newArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
