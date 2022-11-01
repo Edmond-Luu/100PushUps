d042a9b0ef8aa837a7022672afe36150d14abd75
Edmond Luu <eluu472@gmail.com>
2022-11-01 09:47:05 -0700

    Finished arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,10 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+    let max = 0;
+    for (let i = 0; i < inputArray.length-1; i++){
+        const diff = Math.abs(inputArray[i] - inputArray[i+1]);
+        max = max < diff ? diff : max;
+    }
+    return max
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
