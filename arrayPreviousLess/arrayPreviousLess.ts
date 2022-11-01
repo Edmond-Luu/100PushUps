b28908911c8ba0e63cc9ec09427561cd53c94c81
Edmond Luu <eluu472@gmail.com>
2022-11-01 10:00:00 -0700

    Finished arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,9 +1,27 @@
 export function arrayPreviousLess(items: number[]): number[] {
-    const prevArray: number[] = [-1]
-    for (let i = 1; i < items.length; i++){
-        prevArray.push(items[i] > items[i-1] ? items[i-1] : -1)
-    }
-    return prevArray;
-}
+        const prevArray: number[] = [-1]
+        for (let i = 1; i < items.length; i++){
+            prevArray.push(items[i] > items[i-1] ? items[i-1] : -1)
+        }
+        return prevArray;
 
+
+    
+    // const lessThanList: number[] = [];
+
+    // for (let i = items.length - 1; i >= 0; i--){
+    //     for(let j = i; j >= 0; j--){
+    //         if(items[i] > items[j]){
+    //             lessThanList.unshift(items[j]);
+    //             break;
+    //         } else if (j===0){
+    //             lessThanList.unshift(-1);
+    //             break;
+    //         }
+    //     }
+    // }
+    // return lessThanList;
+
+    
+}
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
