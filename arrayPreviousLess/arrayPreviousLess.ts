d70ad917fe857ee1e9cf901b414cb8ff26ba3329
Edmond Luu <eluu472@gmail.com>
2022-11-01 09:54:22 -0700

    Finished arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,9 @@
 export function arrayPreviousLess(items: number[]): number[] {
-
+    const prevArray: number[] = [-1]
+    for (let i = 1; i < items.length; i++){
+        prevArray.push(items[i] > items[i-1] ? items[i-1] : -1)
+    }
+    return prevArray;
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
