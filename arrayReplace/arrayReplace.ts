7b6b134a6ac0afceb8b6af06486e0a33b9a18278
Edmond Luu <eluu472@gmail.com>
2022-11-01 10:06:45 -0700

    Finished arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,14 +1,17 @@
 export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-    const newArray: number[] = [];
 
-    for (let i = 0; i < inputArray.length; i++){
-        if (inputArray[i] === elemToReplace){
-            newArray.push(substitutionElem)
-        } else{
-            newArray.push(inputArray[i]);
-        }
+    for (let i = 0; i < inputArray.length; i++) {
+        inputArray[i] = inputArray[i] === elemToReplace ? substitutionElem : inputArray[i];
     }
-    return newArray;
+    return inputArray;
+
+    // inputArray.forEach((element,index) => {
+    //     if(element === elemToReplace){
+    //         inputArray[index] = substitutionElem;
+    //     }
+    // });
+    // return inputArray;
+    
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
