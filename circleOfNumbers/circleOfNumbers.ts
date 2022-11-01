02fda97a7d9bda9bc2953dbb32abf6d42a2cc3f2
Edmond Luu <eluu472@gmail.com>
2022-10-31 18:20:14 -0700

    Finished circleOfNumbers

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,14 @@
 export function circleOfNumbers(n: number, firstNumber: number): number {
-
+    const circle: number[] = []
+    for (let i = 0; i<n; i++){
+        circle.push(i);
+    }
+    const oppositeIndex = (n/2) + firstNumber
+    if (oppositeIndex >= n){
+        return circle[n-oppositeIndex];
+    } else{
+        return circle[oppositeIndex];
+    }
 }
 
-// console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 2));
