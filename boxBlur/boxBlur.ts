2766af365517d956f79822a1621c81197d9d3f22
Edmond Luu <eluu472@gmail.com>
2022-11-02 10:06:18 -0700

    Finished boxBlue

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,7 +1,17 @@
 export function boxBlur(image: number[][]): number[][] {
-
+    let length:number = 0;
+    let total:number = 0;
+    const final:number[][] = [];
+    for (let array=0; array<image.length; array++){
+        for(let i=0; i<image[array].length; i++){
+            length++;
+            total += image[array][i]
+        }
+    }
+    final.push([Math.floor(total/length])
+    return final;
 }
 
-// console.log(boxBlur([[1, 1, 1], 
-//     [1, 7, 1], 
-//     [1, 1, 1]]));
+console.log(boxBlur([[1, 1, 1], 
+    [1, 7, 1], 
+    [1, 1, 1]]));
