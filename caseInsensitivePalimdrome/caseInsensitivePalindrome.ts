05608a8a3f266ec60172a38a3fbbaeba1a3ed8a1
Edmond Luu <eluu472@gmail.com>
2022-11-02 10:20:59 -0700

    Finished caseInsensitivePalindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,8 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+    const lowercase = inputString.toLowerCase();
+    const reverse = lowercase.split("").reverse().join("")
+    return lowercase === reverse;
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abac'));
