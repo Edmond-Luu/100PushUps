655cdd61c75af47c514123b10bf2be3e2bceda6e
Edmond Luu <eluu472@gmail.com>
2022-11-02 10:23:36 -0700

    Finished boxBlur

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,39 +1,39 @@
 export function boxBlur(image: number[][]): number[][] {
-        let length:number = 0;
-        let total:number = 0;
-        const final:number[][] = [];
-        for (let array=0; array<image.length; array++){
-            for(let i=0; i<image[array].length; i++){
-                length++;
-                total += image[array][i]
+        // let length:number = 0;
+        // let total:number = 0;
+        // const final:number[][] = [];
+        // for (let array=0; array<image.length; array++){
+        //     for(let i=0; i<image[array].length; i++){
+        //         length++;
+        //         total += image[array][i]
+        //     }
+        // }
+        // final.push([Math.floor(total/length])
+        // return final;
+
+
+
+    const res = [];
+
+    for (let y = 0; y < image.length - 2; y++) {
+        const line = [];
+
+        for (let x = 0; x < image[y].length - 2; x++) {
+            let sum = 0;
+            let count = 0;
+
+            for (let a = y; a < y + 3; a++) {
+                for (let b = x; b < x + 3; b++) {
+                    sum += image[a][b];
+                    count++;
+                }
             }
+            line.push(Math.floor(sum/count));
         }
-        final.push([Math.floor(total/length])
-        return final;
+        res.push(line);
+    }
 
-
-
-    // const res = [];
-
-    // for (let y = 0; y < image.length - 2; y++) {
-    //     const line = [];
-
-    //     for (let x = 0; x < image[y].length - 2; x++) {
-    //         let sum = 0;
-    //         let count = 0;
-
-    //         for (let a = y; a < y + 3; a++) {
-    //             for (let b = x; b < x + 3; b++) {
-    //                 sum += image[a][b];
-    //                 count++;
-    //             }
-    //         }
-    //         line.push(Math.floor(sum/count));
-    //     }
-    //     res.push(line);
-    // }
-
-    // return res;
+    return res;
 
 
 
