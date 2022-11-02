0da634b27051948107d6bc8453210a4f7a8e8357
Edmond Luu <eluu472@gmail.com>
2022-11-02 10:14:59 -0700

    Finished candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,9 +1,8 @@
 export function candies(n: number, m: number): number {
-    // return (Math.floor(m/n))*n;
+    return (Math.floor(m/n))*n;
 
-    const candy = Math.floor(m / n);
-    return candy * n;
-
+    // const candy = Math.floor(m / n);
+    // return candy * n;
 }
 
 console.log(candies(3, 10));
