5f4ecf38628e8123d1698a39e78cd39868e4e0ca
Edmond Luu <eluu472@gmail.com>
2022-11-03 09:40:10 -0700

    Finished characterParityy

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,12 @@
 export function characterParity(symbol: string): string {
-
+    const integer = parseInt(symbol)
+    if (isNaN(integer)) {
+        return "not a digit"
+    }
+    return integer % 2 === 0? "even": "odd"
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity('5'))
+console.log(characterParity('8'))
+console.log(characterParity('q'))
+
