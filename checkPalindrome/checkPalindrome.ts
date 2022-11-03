9228371bc1b646ef303ccafbb27986394f340016
Edmond Luu <eluu472@gmail.com>
2022-11-03 09:46:37 -0700

    Finished checkPalindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -2,6 +2,12 @@ export function checkPalindrome(inputString: string): boolean {
     const lowerCased = inputString.toLowerCase();
     const reversed = lowerCased.split("").reverse().join("");
     return reversed === lowerCased;
+
+    // const originalLowerCase = inputString.toLowerCase();
+    // return originalLowerCase.split("").reverse().join("") === originalLowerCase;
+
+
+
 }
 
 console.log(checkPalindrome('aabaa'));
