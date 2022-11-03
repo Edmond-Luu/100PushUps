a5e92e7ee731b0d94b3d52ca19ce8ab4948f4d4f
Edmond Luu <eluu472@gmail.com>
2022-11-03 09:35:00 -0700

    Finished centuryFromYear

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,14 @@
 export function centuryFromYear(year: number): number {
+    const string = year.toString()
+    if (string[string.length-1] === "0"){
+        return Math.floor(year/100);
+    } else{
+        return Math.floor(year/100) + 1
+    }
 
+
+    
 }
 
-// console.log(centuryFromYear(1905));
-// console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
