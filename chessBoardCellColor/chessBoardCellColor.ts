4198563e3a94cb5b302e369775601473cfe3306c
Edmond Luu <eluu472@gmail.com>
2022-11-03 13:49:36 -0700

    chessBoardCellColour

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,5 +1,5 @@
 export function chessBoardCellColor(cell1: string, cell2: string): boolean {
-    const xCoord = {
+    const board = {
         "a": 1,
         "b": 2,
         "c": 3,
@@ -10,17 +10,17 @@ export function chessBoardCellColor(cell1: string, cell2: string): boolean {
         "h": 8
     }
 
-    const cell1xNum: number = xCoord[cell1[0].toLowerCase()];
+    const cell1xNum: number = board[cell1[0].toLowerCase()];
     const cell1yNum: number = parseInt(cell1[1])
     const cell1Total: number = cell1xNum + cell1yNum;
 
-    const cell2xNum: number = xCoord[cell2[0].toLowerCase()];
+    const cell2xNum: number = board[cell2[0].toLowerCase()];
     const cell2yNum: number = parseInt(cell2[1])
     const cell2Total: number = cell2xNum + cell2yNum;
 
     return cell1Total % 2 === cell2Total % 2;
 
-    
+
 }
 
 // console.log(chessBoardCellColor('A1', 'C3'));
