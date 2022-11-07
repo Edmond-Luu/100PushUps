2047c536a4ad74acb05e7a97139989e64417b0c6
Edmond Luu <eluu472@gmail.com>
2022-11-07 11:41:43 -0800

    Finished confirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,9 @@
 export function confirmEnding(str: string, target: string) {
-
+    const strArr = str.split("");
+    const targetArr = target.split("");
+    strArr.splice(0, str.length - target.length);
+    return strArr.join("") === targetArr.join("");
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+console.log(confirmEnding("Open sesame", "pen"));
