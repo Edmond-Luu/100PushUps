1b7e8536d0f2247810aa723b625e57faeb754f8c
Edmond Luu <eluu472@gmail.com>
2022-11-07 11:44:04 -0800

    Finished confirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,8 +1,11 @@
 export function confirmEnding(str: string, target: string) {
-    const strArr = str.split("");
-    const targetArr = target.split("");
-    strArr.splice(0, str.length - target.length);
-    return strArr.join("") === targetArr.join("");
+    // const strArr = str.split("");
+    // const targetArr = target.split("");
+    // strArr.splice(0, str.length - target.length);
+    // return strArr.join("") === targetArr.join("");
+
+    const start = str.length - target.length;
+    return str.substring(start, str.length) === target;
 }
 
 console.log(confirmEnding("Abstraction", "action"));
