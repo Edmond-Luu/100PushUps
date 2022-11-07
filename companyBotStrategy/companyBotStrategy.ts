a8365a70354ab002ad2a568107418f09e2d45cf1
Edmond Luu <eluu472@gmail.com>
2022-11-07 10:47:12 -0800

    Finished companyBotStrategy

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,20 +1,32 @@
 export function companyBotStrategy(trainingData: number[][]): number {
-    let correctTime: number = 0;
-    let correctCount: number = 0;
+    // let correctTime: number = 0;
+    // let correctCount: number = 0;
 
-    for (let array of trainingData) {
-        if (array[1] === 1) {
-            correctTime += array[0];
-            correctCount++;
+    // for (let array of trainingData) {
+    //     if (array[1] === 1) {
+    //         correctTime += array[0];
+    //         correctCount++;
+    //     }
+    // }
+    // if (correctTime / correctCount) {
+    //     return correctTime / correctCount
+    // } else {
+    //     return 0;
+    // }
+
+
+
+
+    let time = 0;
+    let correctness = 0;
+
+    trainingData.forEach((data: number[]) => {
+        if (data[1] === 1) {
+            time += data[0];
+            correctness += data[1];
         }
-    }
-    if (correctTime / correctCount) {
-        return correctTime / correctCount
-    } else {
-        return 0;
-    }
-
-    
+    });
+    return time / correctness || 0;
 }
 
 console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
