43c59cc77ef500eb1d440488e7c102c1f6328b46
Edmond Luu <eluu472@gmail.com>
2022-11-07 10:49:06 -0800

    Finished companyBotStrategy

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,13 @@
 export function compareIntegers(a: string, b: string): string {
+    if (parseInt(a) === parseInt(b)) {
+        return "equal";
+    } else if (parseInt(a) > parseInt(b)) {
+        return "greater";
+    } else {
+        return "less";
+    };
+};
 
-}
-
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));
