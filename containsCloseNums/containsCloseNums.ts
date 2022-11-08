0887a8b5cdd59f91f4480362132bdf7b98fdd46c
Edmond Luu <eluu472@gmail.com>
2022-11-08 10:04:04 -0800

    containsCloseNums

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,5 +1,10 @@
 export function containsCloseNums(nums: number[], k: number): boolean {
-
+    for (let i=1; i<k; i++){
+        if (nums[i] === nums[i + k]){
+            return true
+        }
+    }
+    return false;
 };
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
