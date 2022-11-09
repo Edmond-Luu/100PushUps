882722e05797bd4fb4fadd45109a3f13af04f7da
Edmond Luu <eluu472@gmail.com>
2022-11-09 09:49:25 -0800

    Finished differentSymbolsNative

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,8 @@
 export function differentSymbolsNaive(s: string): number {
-
+    const uniqueChars: string[] = [];
+    const sArr = s.split("");
+    sArr.forEach(letter => !uniqueChars.includes(letter) ? uniqueChars.push(letter) : "");
+    return uniqueChars.length;
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive('cabca'));
