98b50df38e0a54a93eebc9db7affe1919755ec69
Edmond Luu <eluu472@gmail.com>
2022-11-09 10:39:15 -0800

    Finished domainType

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,16 @@
 export function domainType(domains: string[]): string[] {
+    const domainResults: string[] = [];
+    const domainTypes = {
+        com: "commercial",
+        org: "organization",
+        net: "network",
+        info: "information"
+    };
 
+    for (let i = 0; i < domains.length; i++) {
+        domainResults.push(domainTypes[domains[i].split(".").slice(-1)[0]]);
+    };
+    return domainResults;
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
