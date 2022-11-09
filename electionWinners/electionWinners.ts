57ec300b3854272ed92d772f48d5731b76a7d222
Edmond Luu <eluu472@gmail.com>
2022-11-09 15:05:46 -0800

    Finished electionWinners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,10 @@
 export function electionsWinners(votes: number[], k: number): number {
+    const winners: numbers[] = [];
+    const currentWinner = Math.max(...votes);
 
+    votes.forEach(candidate => candidate + k > currentWinner ? winners.push(candidate) : "")
+
+    return winners.length;
 }
 
-// console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 3));
