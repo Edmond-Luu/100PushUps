6feeaa1e8f630deb88ca0e0df9e2d34bb00feb54
Edmond Luu <eluu472@gmail.com>
2022-11-11 16:41:24 -0800

    Finished fareEstimator

--- fareEstimator/fareEstimator.ts
+++ fareEstimator/fareEstimator.ts
@@ -1,17 +1,22 @@
 export function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
-    const timeFares: number[] = [];
-    const distFares: number[] = [];
-    const totalFares: number[] = [];
-    for (let i = 0; i < cost_per_minute.length; i++) {
-        timeFares.push(cost_per_minute[i] * ride_time);
-    };
-    for (let i = 0; i < cost_per_mile.length; i++) {
-        distFares.push(cost_per_mile[i] * ride_distance);
-    };
-    for (let i = 0; i < timeFares.length; i++) {
-        totalFares.push((timeFares[i] + distFares[i]));
-    };
+    // const timeFares: number[] = [];
+    // const distFares: number[] = [];
+    // const totalFares: number[] = [];
+    // for (let i = 0; i < cost_per_minute.length; i++) {
+    //     timeFares.push(cost_per_minute[i] * ride_time);
+    // };
+    // for (let i = 0; i < cost_per_mile.length; i++) {
+    //     distFares.push(cost_per_mile[i] * ride_distance);
+    // };
+    // for (let i = 0; i < timeFares.length; i++) {
+    //     totalFares.push((timeFares[i] + distFares[i]));
+    // };
 
+    // return totalFares;
+
+    const totalFares = cost_per_minute.map((item, index) =>{
+        return (item * ride_time) + (ride_distance * cost_per_mile[index]);
+    });
     return totalFares;
 };
 
