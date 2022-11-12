bb1508b20b465c56957a96a8766956b746263bfc
Edmond Luu <eluu472@gmail.com>
2022-11-12 10:11:12 -0800

    Finished fareEstimator

--- fareEstimator/fareEstimator.ts
+++ fareEstimator/fareEstimator.ts
@@ -5,7 +5,6 @@ export function fareEstimator(ride_time: number, ride_distance: number, cost_per
     // return totalFares;
 
 
-    
     const fareCost: number[] = [];
     for (let i = 0; i < cost_per_mile.length; i++) {
         fareCost.push(ride_time * cost_per_minute[i] + ride_distance * cost_per_mile[i])
