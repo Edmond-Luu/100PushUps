1931f9f5e6d5518c9aa4b57d79f9b666312c4f50
Edmond Luu <eluu472@gmail.com>
2022-11-15 10:11:12 -0800

    Finished findEmailDomain

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,6 @@
 export function findEmailDomain(address: string): string {
-
+    return address.split("@").slice(-1)[0];
 }
 
-// console.log(findEmailDomain('prettyandsimple@example.com'));
-// console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain('prettyandsimple@example.com'));
+console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
