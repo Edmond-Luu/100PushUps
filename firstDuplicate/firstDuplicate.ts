bb80d14fb3e063b031f5e7d64a0dd9851abdb03b
Edmond Luu <eluu472@gmail.com>
2022-11-15 16:30:22 -0800

    Finished firstDuplicate

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,6 +1,14 @@
 export function firstDuplicate(a: number[]): number {
+    const arrayValues = [];
+    for (let i = 0; i < a.length; i++) {
+        if (!arrayValues.includes(a[i])) {
+            arrayValues.push(a[i]);
+        } else {
+            return a[i];
+        };
+    };
+    return -1;;
+};
 
-}
-
-// console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
-// console.log(firstDuplicate([2, 4, 3, 5, 1]));
+console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.log(firstDuplicate([2, 4, 3, 5, 1]));
