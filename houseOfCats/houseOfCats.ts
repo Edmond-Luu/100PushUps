296eb7e651ce5d502e75a5d42ebf2e670d3a374a
Edmond Luu <eluu472@gmail.com>
2022-11-18 10:05:36 -0800

    Finished houseOfCats

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,13 @@
 export function houseOfCats(legs: number): number[] {
+    const peopleCount = [];
 
+    peopleCount.push((legs % 4)/2)
+
+    peopleCount.push((legs/4))
+
+
+    return peopleCount
 }
 
-// console.log(houseOfCats(6));
-// console.log(houseOfCats(2));
+console.log(houseOfCats(6));
+console.log(houseOfCats(2));
