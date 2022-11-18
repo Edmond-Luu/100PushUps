da7b27ddfcc93551f979a2df5be28056656c2966
Edmond Luu <eluu472@gmail.com>
2022-11-17 16:40:09 -0800

    Finished flattenArray

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,14 +1,31 @@
 export function flattenArray(arr: any[]): any[] {
-    const finalArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        finalArr.push(arr[i]);
+    // const finalArr = [];
+    // for (let i = 0; i < arr.length; i++) {
+    //     finalArr.push(arr[i]);
+    // };
+    // for (let i = 0; i < finalArr.length; i++) {
+    //     while(Array.isArray(finalArr[i])){
+    //         finalArr.splice(i, 1, ...finalArr[i])
+    //     }
+    // };
+    // return finalArr;
+
+
+    const oneArray = [];
+
+    flatten(arr);
+
+    function flatten(arr: any[]) {
+        arr.forEach(element => {
+            if (Array.isArray(element)) {
+                flatten(element);
+            } else {
+                oneArray.push(element);
+            }
+        });
     };
-    for (let i = 0; i < finalArr.length; i++) {
-        while(Array.isArray(finalArr[i])){
-            finalArr.splice(i, 1, ...finalArr[i])
-        }
-    };
-    return finalArr;
-}
+
+    return oneArray;
+};
 console.log(flattenArray([[["a"]], [["b"]]]));
 console.log(flattenArray([1, [2], [3, [[4]]]]));
