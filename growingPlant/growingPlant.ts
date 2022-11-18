003abb5e9acd7f7fd710b472c327cf027d98a7df
Edmond Luu <eluu472@gmail.com>
2022-11-17 16:46:02 -0800

    Finished growingPlants

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,17 @@
 export function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+    let days = 0;
+    let currentHeight = 0;
 
-}
+    while (currentHeight < desiredHeight) {
+        currentHeight += upSpeed;
+        if (currentHeight < desiredHeight) {
+            currentHeight -= downSpeed;
+        }
+        days++;
+    }
 
-// console.log(growingPlant(100, 10, 910))
+
+    return days;
+};
+
+console.log(growingPlant(100, 10, 910))
