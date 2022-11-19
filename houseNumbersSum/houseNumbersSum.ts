b8db859cebbbe56254bc7305ca959e1950ea0fda
Edmond Luu <eluu472@gmail.com>
2022-11-19 10:11:12 -0800

    Finished houseNumbersSum

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,14 +1,24 @@
 export function houseNumbersSum(inputArray: number[]): number {
-    const addedArr = [];
-    for (let i = 0; i < inputArray.length; i++) {
-        if (inputArray[i] === 0) {
-            break;
-        } else {
-            addedArr.push(inputArray[i])
+    // const addedArr = [];
+    // for (let i = 0; i < inputArray.length; i++) {
+    //     if (inputArray[i] === 0) {
+    //         break;
+    //     } else {
+    //         addedArr.push(inputArray[i])
+    //     }
+    // }
+
+    // return addedArr.reduce((a, b) => a + b);
+
+    let total = 0;
+
+    for(let i = 0; i < inputArray.length; i++){
+        if(inputArray[i] === 0){
+            return total;
+        } else{
+            total += inputArray[i]
         }
     }
-
-    return addedArr.reduce((a, b) => a + b);
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
