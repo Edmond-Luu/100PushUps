189817aecdeea6b21c39b7c2a30c2c746dc033e3
Edmond Luu <eluu472@gmail.com>
2022-11-21 17:05:09 -0800

    Finished htmlEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,16 @@
 export function htmlEndTagByStartTag(startTag: string): string {
+    // const elementTag = startTag.replace(">","").split(" ")[0].split("");
+    // elementTag.splice(1,0,"/");
+    // elementTag.push(">");
+    // const endingTag = elementTag.join("");
+    // return endingTag;
 
+    const elementTag = startTag.split(" ")[0].split("");
+    elementTag.splice(1,0,"/");
+    const endingTag = elementTag.join("");
+    return endingTag[endingTag.length -1 ] === ">" ? endingTag : endingTag + ">";
+    
 }
 
-// console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-// console.log(htmlEndTagByStartTag('<i>'));
+console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+console.log(htmlEndTagByStartTag('<i>'));
