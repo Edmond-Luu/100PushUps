96dde2fd2301350039deb3c610c74bcc68722509
Edmond Luu <eluu472@gmail.com>
2022-11-22 16:28:30 -0800

    Finished htmlEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -5,11 +5,25 @@ export function htmlEndTagByStartTag(startTag: string): string {
     // const endingTag = elementTag.join("");
     // return endingTag;
 
-    const elementTag = startTag.split(" ")[0].split("");
-    elementTag.splice(1,0,"/");
-    const endingTag = elementTag.join("");
-    return endingTag[endingTag.length -1 ] === ">" ? endingTag : endingTag + ">";
-    
+
+
+    // const elementTag = startTag.split(" ")[0].split("");
+    // elementTag.splice(1,0,"/");
+    // const endingTag = elementTag.join("");
+    // return endingTag[endingTag.length -1 ] === ">" ? endingTag : endingTag + ">";
+
+
+
+    const splitString = startTag.split(" ");
+    const splitTag = splitString[0].toString().split("");
+    let endTag = '</'
+
+    for (let i = 1; i < splitTag.length; i++) {
+        endTag += splitTag[i];
+    };
+
+    endTag = endTag[endTag.length - 1] === '>' ? endTag : endTag += ">"
+    return endTag;
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
