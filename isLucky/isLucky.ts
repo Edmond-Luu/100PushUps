3e0e5afb6373d48b2492ee9e3900f5415988fc9f
Edmond Luu <eluu472@gmail.com>
2022-11-24 10:11:12 -0800

    Finished isLucky

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,6 +1,12 @@
 export function isLucky(n: number): boolean {
-
+    let nArr: (number | string)[] = n.toString().split("")
+    nArr = nArr.map(num => parseInt(num))
+    const firstHalfArr = nArr.slice(0, nArr.length / 2)
+    const lastHalfArr = nArr.slice(nArr.length / 2, nArr.length)
+    const firstHalfSum = firstHalfArr.reduce((a, b) => a + b);
+    const lastHalfSum = lastHalfArr.reduce((a, b) => a + b);
+    return firstHalfSum === lastHalfSum;
 }
 
-// console.log(isLucky(1230));
-// console.log(isLucky(239017));
+console.log(isLucky(1230));
+console.log(isLucky(239017));
