b2d2a23470cadf8f124789449e3ca9c337ec6b50
Edmond Luu <eluu472@gmail.com>
2022-11-24 10:25:59 -0800

    Finished integerToStringOfFixedWidth

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,35 +1,46 @@
 export function integerToStringOfFixedWidth(number: number, width: number): string {
-    // let newStr = [];
-    // const strArr = number.toString().split("");
-    // let strLength = strArr.length;
-    // for (let i = 0; i < width; i++) {
-    //     while (strLength < width) {
-    //         strArr.unshift("0")
-    //         strLength = strArr.length
-    //     }
-    //     newStr.push(strArr[i])
-    // }
-    // return newStr.join("");
+    let newStr = [];
+    const strArr: string[] = number.toString().split("");
+    let strLength = strArr.length;
+    if (width === strLength) {
+        return number.toString();
+    } else {
+        if (width > strLength) {
+            while (strLength < width) {
+                strArr.unshift("0")
+                strLength = strArr.length
+            }
 
-
-
-    let stringifiedNum = number.toString();
-    const numberWidth = stringifiedNum.length;
-    const widthDiff = width - numberWidth;
-
-    if (numberWidth < width) {
-        let padStart = "";
-        for (let i = 0; i < widthDiff; i++) {
-            padStart = padStart.concat("0");
+            for (let i = 0; i < width; i++) {
+                newStr.push(strArr[i])
+            }
         }
-        return padStart.concat(stringifiedNum);
-    };
-
-    if (numberWidth > width) {
-        return stringifiedNum.substring(widthDiff, stringifiedNum.length)
+        if (width < strLength) {
+            return strArr.slice(strLength - width).join("")
+        }
     }
+    
+    return newStr.join("");
 
-    return stringifiedNum;
+
+
+    // let stringifiedNum = number.toString();
+    // const numberWidth = stringifiedNum.length;
+    // const widthDiff = width - numberWidth;
+
+    // if (numberWidth < width) {
+    //     let padStart = "";
+    //     for (let i = 0; i < widthDiff; i++) {
+    //         padStart = padStart.concat("0");
+    //     }
+    //     return padStart.concat(stringifiedNum);
+    // };
+
+    // if (numberWidth > width) {
+    //     return stringifiedNum.substring(widthDiff, stringifiedNum.length)
+    // }
+
+    // return stringifiedNum;
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
