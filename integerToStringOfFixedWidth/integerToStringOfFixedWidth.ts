224a739352d1171b058c2dd436bc29acf3b81318
Edmond Luu <eluu472@gmail.com>
2022-11-23 16:37:36 -0800

    Finished integerToStringOfFixedWidth

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -2,20 +2,14 @@ export function integerToStringOfFixedWidth(number: number, width: number): stri
     let newStr = [];
     const strArr = number.toString().split("");
     let strLength = strArr.length;
-    if (strLength === width) {
-        return number;
-    }  
-    for(let i = 0; i < width; i++){
-        while(strLength < width){
+    for (let i = 0; i < width; i++) {
+        while (strLength < width) {
             strArr.unshift("0")
             strLength = strArr.length
         }
         newStr.push(strArr[i])
-    }    
+    }
     return newStr.join("");
-
-
-
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
