8d63397d1b418b5a6dd3a3e2c90cc1d285511386
Edmond Luu <eluu472@gmail.com>
2022-11-25 10:11:12 -0800

    Finished incrementalBackups

--- incrementalBackups/incrementalBackups.ts
+++ incrementalBackups/incrementalBackups.ts
@@ -1,10 +1,24 @@
 export function incrementalBackups(lastBackupTime: number, changes: number[][]): number[] {
-    const backupSet = new Set
-    changes = changes.filter(file => file[0] > lastBackupTime)
-    for (let file of changes) {
-        backupSet.add(file[1])
-    }
-    return [...backupSet].sort((a, b) => a - b);
+    // const backupSet = new Set();
+    // changes = changes.filter(file => file[0] > lastBackupTime)
+    // for (let file of changes) {
+    //     backupSet.add(file[1])
+    // }
+    // return [...backupSet].sort((a, b) => a - b);
+
+
+
+    const fileIds: number[] = [];
+    for (let change of changes) {
+        const fileBackupTime = change[0];
+        const fileId = change[1];
+        if (fileBackupTime > lastBackupTime) {
+            if (!fileIds.includes(fileId)) {
+                fileIds.push(fileId)
+            }
+        };
+    };
+    return fileIds.sort((fileId1, fileId2) => fileId1 - fileId2)
 }
 
 console.log(incrementalBackups(461620205, [[461620203, 1],
