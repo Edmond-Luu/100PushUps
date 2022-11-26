65ef16537c1fd4116d866a559dd9d3b99615e4a2
Edmond Luu <eluu472@gmail.com>
2022-11-26 10:11:12 -0800

    Finished isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,9 @@
 export function isTandemRepeat(inputString: string): boolean {
-
+    const firstHalf = inputString.slice(0, inputString.length / 2)
+    const secondHalf = inputString.slice(inputString.length / 2, inputString.length)
+    return firstHalf === secondHalf
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'))
+console.log(isTandemRepeat('qqq'))
+console.log(isTandemRepeat('2w2ww'))
