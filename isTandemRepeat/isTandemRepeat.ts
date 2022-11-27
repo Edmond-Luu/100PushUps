b2732d70711437bd8e3c210f678a0b4a1fc4bef7
Edmond Luu <eluu472@gmail.com>
2022-11-27 10:11:12 -0800

    Finished isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,10 +1,19 @@
 export function isTandemRepeat(inputString: string): boolean {
-    if(inputString.length % 2 !== 0){
-        return false;
+    // if(inputString.length % 2 !== 0){
+    //     return false;
+    // }
+    // const firstHalf = inputString.slice(0, inputString.length / 2)
+    // const secondHalf = inputString.slice(inputString.length / 2, inputString.length)
+    // return firstHalf === secondHalf
+
+
+    const wordLength = inputString.length;
+    if (wordLength % 2 === 0){
+        const firstHalf = inputString.slice(0, wordLength / 2);
+        const secondHalf = inputString.slice(wordLength / 2, wordLength);
+        return firstHalf === secondHalf;
     }
-    const firstHalf = inputString.slice(0, inputString.length / 2)
-    const secondHalf = inputString.slice(inputString.length / 2, inputString.length)
-    return firstHalf === secondHalf
+    return false;
 }
 
 console.log(isTandemRepeat('tandemtandem'))
