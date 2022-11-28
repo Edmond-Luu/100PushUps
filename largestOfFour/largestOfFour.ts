28863080d7c2fb5fce62030aec154885078c7021
Edmond Luu <eluu472@gmail.com>
2022-11-28 10:11:12 -0800

    Finished largestOfFour

--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,9 +1,20 @@
 export function largestOfFour(nums: number[][]): number[] {
-    const largestNums = [];
-    nums.map(arr => {
-        largestNums.push(arr.sort((a, b) => b - a)[0])
-    })
-    return largestNums;
+    // const largestNums = [];
+    // nums.map(arr => {
+    //     largestNums.push(arr.sort((a, b) => b - a)[0])
+    // })
+    // return largestNums;
+
+
+    const highestNumbers: number[] = []
+    for(let numberGroup of nums){
+        let largest = 0;
+        for(let number of numberGroup){
+            largest = largest < number ? number : largest;
+        }
+        highestNumbers.push(largest);
+    }
+    return highestNumbers
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
