4778d58ce2c075dc3ebae53d1e5d01810bbf7a53
Edmond Luu <eluu472@gmail.com>
2022-11-28 16:28:15 -0800

    Finished largestNumber

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,9 +1,17 @@
 export function largestNumber(n: number): number {
-    let largest = "";
-    for (let i = 0; i < n; i++){
-        largest += "9"
+    // let largest = "";
+    // for (let i = 0; i < n; i++){
+    //     largest += "9"
+    // }
+    // return parseInt(largest)
+
+
+
+    let word = "";
+    for (let i = 0; i < n; i++) {
+        word = word.concat("9");
     }
-    return parseInt(largest)
+    return parseInt(word);
 }
 
 console.log(largestNumber(2));
