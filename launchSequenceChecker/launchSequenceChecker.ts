5d65bd1e15695a688a79f5bd96ad3b87fc5bbb73
Edmond Luu <eluu472@gmail.com>
2022-11-29 16:36:16 -0800

    Finished launchSequenceChecker

--- launchSequenceChecker/launchSequenceChecker.ts
+++ launchSequenceChecker/launchSequenceChecker.ts
@@ -1,6 +1,16 @@
 export function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
-
+    const launchCodes = {};
+    for (let i = 0; i < systemNames.length; i++) {
+        if (!launchCodes.hasOwnProperty(systemNames[i])) {
+            launchCodes[systemNames[i]] = stepNumbers[i]
+        } else {
+            if (launchCodes[systemNames[i]] > stepNumbers[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
-// console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
-// console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
+console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
+console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
