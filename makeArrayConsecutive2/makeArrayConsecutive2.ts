6054273164d5b84c34387e37af2f32e87d9763ee
Edmond Luu <eluu472@gmail.com>
2022-11-30 16:48:32 -0800

    Finished makeArrayConsecutive2

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,10 @@
 export function makeArrayConsecutive2(statues: number[]): number {
-
+    const missingStatues = [];
+    statues.sort((a, b) => a - b);
+    for(let i = statues[0]; i < statues[statues.length - 1]; i++){
+        !statues.includes(i) ? missingStatues.push(i) : ""
+    }
+    return missingStatues.length;
 }
 
-// console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([6, 2, 3, 8]));
