e8b79ebacc48f6d80795f49d5d956ab718ebad55
Edmond Luu <eluu472@gmail.com>
2022-11-30 16:39:56 -0800

    Finished longestDigitsPrefix

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,13 +1,17 @@
 export function longestDigitsPrefix(inputString: string): string {
-    const splitStr = inputString.split("");
-    let digitsStr = ""
-    for (let i = 0; i < splitStr.length; i++) {
-        if (parseInt(splitStr[i])) {
-            digitsStr += splitStr[i]
-        } else {
-            return digitsStr
-        }
-    }
+    // const splitStr = inputString.split("");
+    // let digitsStr = ""
+    // for (let i = 0; i < splitStr.length; i++) {
+    //     if (parseInt(splitStr[i])) {
+    //         digitsStr += splitStr[i]
+    //     } else {
+    //         return digitsStr
+    //     }
+    // }
+
+
+
+    
 }
 
 console.log(longestDigitsPrefix('123aa1'));
