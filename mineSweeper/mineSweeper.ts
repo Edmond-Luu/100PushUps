eff4b7c75260aacf2a4dd533586f65a861bd298c
Edmond Luu <eluu472@gmail.com>
2022-12-04 12:00:00 -0800

    mineSweeper

--- mineSweeper/mineSweeper.ts
+++ mineSweeper/mineSweeper.ts
@@ -1,7 +1,18 @@
 export function minesweeper(matrix: boolean[][]): number[][] {
+    const board: [][] = [];
+    for (let i = 0; i < matrix.length; i++) {
+        board.push([])
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            matrix[i][j] === true ? board[i].push(1) : board[i].push(2);
+        }
+    }
+    return board;
 
 }
 
-// console.log(minesweeper([[true, false, false],
-// [false, true, false],
-// [false, false, false]]));
+console.log(minesweeper([[true, false, false],
+[false, true, false],
+[false, false, false]]));
