d86f2aa06348c7ed694c6806946e6725980db60b
Edmond Luu <eluu472@gmail.com>
2022-12-04 12:00:00 -0800

    missingLetters

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,14 +1,32 @@
 export function missingLetters(str: string): string {
-    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const splitStr = str.split("")
-    const shortAlphabet = alphabet.splice(alphabet.indexOf(splitStr[0]), splitStr.length)
-    for (let i = 0; i < splitStr.length; i++) {
-        if (splitStr[i] !== shortAlphabet[i]) {
-            return shortAlphabet[i]
+    // const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    // const splitStr = str.split("")
+    // const shortAlphabet = alphabet.splice(alphabet.indexOf(splitStr[0]), splitStr.length)
+    // if (alphabet.join("") === str)
+    //     for (let i = 0; i < splitStr.length; i++) {
+    //         if (splitStr[i] !== shortAlphabet[i]) {
+    //             return shortAlphabet[i]
+    //         }
+    //     }
+    // return "undefined";
+
+
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split("");
+    const chars = str.split("");
+
+    if (chars[0] !== "a") {
+        return undefined;
+    }
+
+    for (let i = 0; i < chars.length; i++) {
+        if (chars[i] !== alphabet[i]) {
+            return alphabet[i]
         }
     }
-    return "undefined";
-}
+
+    return undefined;
+};
 
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
