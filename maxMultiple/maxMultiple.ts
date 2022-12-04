a2b9bb8fa3847b2bb5cbfe17d56c7b8af7941cc6
Edmond Luu <eluu472@gmail.com>
2022-12-04 12:00:00 -0800

    maxMultiple

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,9 +1,14 @@
 export function maxMultiple(divisor: number, bound: number): number {
-    for (let i = bound; i > 0; i--) {
-        if (i % divisor === 0) {
-            return i;
-        }
-    }
-}
+    // for (let i = bound; i > 0; i--) {
+    //     if (i % divisor === 0) {
+    //         return i;
+    //     }
+    // }
+
+
+
+    let remainder = bound % divisor;
+    return bound - remainder;
+};
 
 console.log(maxMultiple(3, 10));
