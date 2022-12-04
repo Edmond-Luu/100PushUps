430167620716bd08ba1aa3551e7ee9ea41c4b251
Edmond Luu <eluu472@gmail.com>
2022-12-04 12:00:00 -0800

    minimalNumberOfCoins

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,20 @@
 export function minimalNumberOfCoins(coins: number[], price: number): number {
+    let total = price;
+    let coinUse = 0;
+    const sortedCoins = coins.sort((a, b) => b - a);
 
+    while (total !== 0) {
+        for (let coin of sortedCoins) {
+            if (coin < total) {
+                const amountOfCoins = Math.floor(total / coin)
+                total -= coin * amountOfCoins
+                coinUse += amountOfCoins;
+            } else {
+                continue;
+            }
+        }
+    }
+    return coinUse;
 }
 
-// console.log(minimalNumberOfCoins([1, 2, 10], 28));
+console.log(minimalNumberOfCoins([1, 2, 10], 28));
