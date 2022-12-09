6e3af4f7d3794ab7dc29b44b0da694b18ab33ae3
Edmond Luu <eluu472@gmail.com>
2022-12-08 17:07:50 -0800

    palindromeRearranging

--- palindromeRearranging/palindromeRearranging.ts
+++ palindromeRearranging/palindromeRearranging.ts
@@ -1,35 +1,59 @@
 export function palindromeRearranging(inputString: string): boolean {
-    const splitStr = inputString.split("");
-    const strLength = splitStr.length;
-    const charCounts = {}
-    for (let char of splitStr) {
-        if (!charCounts.hasOwnProperty(char)) {
-            charCounts[char] = 1;
+    // const splitStr = inputString.split("");
+    // const strLength = splitStr.length;
+    // const charCounts = {}
+    // for (let char of splitStr) {
+    //     if (!charCounts.hasOwnProperty(char)) {
+    //         charCounts[char] = 1;
+    //     } else {
+    //         charCounts[char]++
+    //     }
+    // }
+    // const counts = Object.values(charCounts)
+    // const evenOrOdd = counts.map(count => count % 2)
+    // if (strLength % 2 === 0) {
+    //     if evenOrOdd.includes(1){
+    //         return false;
+    //     } else {
+    //         return true;
+    //     }
+    // } else {
+    //     for (let i = 0; i < evenOrOdd.length; i++) {
+    //         if (evenOrOdd[i] === 0) {
+    //             evenOrOdd.splice(i, 1)
+    //             if (evenOrOdd.includes(1)) {
+    //                 return true;
+    //             } else {
+    //                 return false;
+    //             }
+    //         }
+    //         return false;
+    //     }
+    // }
+
+
+
+
+
+    let oddCount = 0;
+    const chars = inputString.split("");
+    const letterObject = {};
+    for (let char of chars) {
+        if (letterObject.hasOwnProperty(chars[char])) {
+            letterObject[chars[char]]++;
         } else {
-            charCounts[char]++
+            letterObject[chars[char]] = 1;
+        };
+    };
+
+    for (let key in letterObject) {
+        if (letterObject[key] % 2 !== 0) {
+            oddCount++
         }
     }
-    const counts = Object.values(charCounts)
-    const evenOrOdd = counts.map(count => count % 2)
-    if (strLength % 2 === 0) {
-        if evenOrOdd.includes(1){
-            return false;
-        } else {
-            return true;
-        }
-    } else {
-        for (let i = 0; i < evenOrOdd.length; i++) {
-            if (evenOrOdd[i] === 0) {
-                evenOrOdd.splice(i, 1)
-                if (evenOrOdd.includes(1)) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            return false;
-        }
-    }
+
+    return oddCount > 1 ? false : true;
+
 }
 
 console.log(palindromeRearranging('aabb'));
