d4854ab97909b285b79ce53546e5240d52c4b6e8
Edmond Luu <eluu472@gmail.com>
2022-12-09 16:28:16 -0800

    pagesNumberingWithInk

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,5 +1,17 @@
 export function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+    let digits = ""
+    const numbers = []
+    for (let i = current; digits.length < numberOfDigits; i++) {
+        digits += `${i}`;
+        numbers.push(i);
+    }
+    if (digits.length !== numberOfDigits) {
+        numbers.splice(numbers.length - 1, 1);
+    }
+    return numbers.sort((a, b) => b - a)[0];
 
 }
 
-// console.log(pagesNumberingWithInk('G'));
+console.log(pagesNumberingWithInk(1, 5));
+console.log(pagesNumberingWithInk(21, 5));
+console.log(pagesNumberingWithInk(8, 4));
