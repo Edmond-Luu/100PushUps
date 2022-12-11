ac7677d23e315b86e88a9a7249f981437ce96d95
Edmond Luu <eluu472@gmail.com>
2022-12-11 12:00:00 -0800

    ratingThresholds

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,24 @@
 export function ratingThreshold(threshold: number, ratings: number[][]): number[] {
+    // const toReview = [];
+    // for (let i = 0; i < ratings.length; i++) {
+    //     let avgScore = 0
+    //     for (let rating of ratings[i]) {
+    //         avgScore += rating / ratings[i].length
+    //     }
+    //     if (avgScore < threshold) {
+    //         toReview.push(i)
+    //     }
+    // }
+    // return toReview;
+
+
+
+
+
 
 }
 
-// console.log(ratingThreshold(3.5, 
-//     [[3, 4],
-//     [3, 3, 3, 4],
-//     [4]]));
+console.log(ratingThreshold(3.5,
+    [[3, 4],
+    [3, 3, 3, 4],
+    [4]]));
