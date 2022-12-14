ceb9145ec46ca7798fe5d16604fc4be958a882a5
Edmond Luu <eluu472@gmail.com>
2022-12-13 16:36:49 -0800

    properNounCorrection

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,7 +1,13 @@
 export function properNounCorrection(noun: string): string {
-    const firstLetter = noun.slice(0,1);
-    const otherLetters = noun.slice(1);
-    return firstLetter.toUpperCase() + otherLetters.toLowerCase();
+    // const firstLetter = noun[0];
+    // const otherLetters = noun.slice(1);
+    // return firstLetter.toUpperCase() + otherLetters.toLowerCase();
+
+
+
+    const firstChar = noun[0].toUpperCase();
+    const restOfWord = noun.slice(1, noun.length).toLowerCase();
+    return firstChar.concat(restOfWord);
 };
 
 console.log(properNounCorrection('pARiS'));
