dc6f99e29f2fd02e69092e08625d69f989b6c0d2
Edmond Luu <eluu472@gmail.com>
2022-12-13 16:52:39 -0800

    reflectString

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -2,15 +2,17 @@ export function reflectString(inputString: string): string {
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     const splitStr = inputString.split("");
-    const newStr = [];
+    let newStr = "";
 
     for (let char of splitStr) {
         const index = alphabet.indexOf(char);
         const newIndex = (index + 1) * -1;
-        const newLetter = alphabet.slice(newIndex)[0];
-        newStr.push(newLetter);
+        newStr += alphabet.slice(newIndex)[0];
+
     };
-    return newStr.join("");
+    return newStr;
+
+    //Suggested solution is too long.
 }
 
 console.log(reflectString("name"));
