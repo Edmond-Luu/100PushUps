a1c8a1f6d537c3bf94e288608b8a60d81de27b79
Edmond Luu <eluu472@gmail.com>
2022-12-13 16:34:40 -0800

    properNounCorrection

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,8 @@
 export function properNounCorrection(noun: string): string {
+    const firstLetter = noun.slice(0,1);
+    const otherLetters = noun.slice(1);
+    return firstLetter.toUpperCase() + otherLetters.toLowerCase();
+};
 
-}
-
-// console.log(properNounCorrection('pARiS'));
-// console.log(properNounCorrection('John'));
+console.log(properNounCorrection('pARiS'));
+console.log(properNounCorrection('John'));
