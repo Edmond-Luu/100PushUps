8c1dc52fe80ff9f9752ddb34c6fc05340823245a
Edmond Luu <eluu472@gmail.com>
2022-12-14 16:35:10 -0800

    seekAndDestroy

--- seekAndDestroy/seekAndDestroy.ts
+++ seekAndDestroy/seekAndDestroy.ts
@@ -1,6 +1,12 @@
 export function seekAndDestroy(arr1: number[], arr2: number[]): number[] {
-
+    const filteredArr = [];
+    for (let num of arr1) {
+        if (!arr2.includes(num)) {
+            filteredArr.push(num);
+        }
+    }
+    return filteredArr;
 }
 
-// console.log(seekAndDestroy([3, 5, 1, 2, 2], [2, 3, 5]));
-// console.log(seekAndDestroy([1, 2, 3, 5, 1, 2, 3], [2, 3]));
+console.log(seekAndDestroy([3, 5, 1, 2, 2], [2, 3, 5]));
+console.log(seekAndDestroy([1, 2, 3, 5, 1, 2, 3], [2, 3]));
