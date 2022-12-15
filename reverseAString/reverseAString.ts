7f471ec0e5f26792be83c7511e64e5280ed96372
Edmond Luu <eluu472@gmail.com>
2022-12-14 16:23:38 -0800

    reverseAString

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,6 +1,9 @@
 export function reverseAString(str: string): string {
-    const splitStr = str.split("").reverse()
-    return splitStr.join("")
+    // const splitStr = str.split("").reverse()
+    // return splitStr.join("")
+
+
+    return str.split("").reverse().join("")
 }
 
 console.log(reverseAString('hello'));
