19950d290fbb2afef4e4be75ca830bf8745202ea
Edmond Luu <eluu472@gmail.com>
2022-12-19 17:02:13 -0800

    stringsConstruction

--- stringsConstruction/stringsConstruction.ts
+++ stringsConstruction/stringsConstruction.ts
@@ -1,5 +1,11 @@
 export function stringsConstruction(a: string, b: string): number {
+    const splitA = a.split("");
+    const splitB = b.split("");
+    let count = 0;
 
+    
+
+    return count;
 }
 
-// console.log(stringsConstruction('abc', 'abccba'));
+console.log(stringsConstruction('abc', 'abccba'));
