5fff4ddaa18de7b11bb96b3cedaa7635fbab7302
Edmond Luu <eluu472@gmail.com>
2022-12-21 12:00:00 -0800

    sumAllPrimes

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,18 +1,34 @@
 export function sumAllPrimes(num: number): number {
-    const primeCheck = (number) => {
-        for (let i = 2; i < number; i++) {
-            if (number % i === 0) {
-                return false;
+    // const primeCheck = (number) => {
+    //     for (let i = 2; i < number; i++) {
+    //         if (number % i === 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // };
+    // let primesTotal = 0;
+    // for (let i = 2; i <= num; i++) {
+    //     if (primeCheck(i)) {
+    //         primesTotal += i;
+    //     };
+    // };
+    // return primesTotal;
+
+
+
+    let primesTotal = 0;
+
+    for (let i = 2; i < num; i++) {
+        for (let j = 2; j <= i; j++) {
+            if (i === j) {
+                primesTotal += i;
+            }
+            if (i % j === 0) {
+                break;
             }
         }
-        return true;
-    };
-    let primesTotal = 0;
-    for (let i = 2; i <= num; i++) {
-        if (primeCheck(i)) {
-            primesTotal += i;
-        };
-    };
+    }
     return primesTotal;
 }
 
