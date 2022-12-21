074f39ceb29e6221f37e8e6274248679917c8745
Edmond Luu <eluu472@gmail.com>
2022-12-20 16:28:52 -0800

    stringsConstruction

--- stringsConstruction/stringsConstruction.ts
+++ stringsConstruction/stringsConstruction.ts
@@ -1,9 +1,21 @@
 export function stringsConstruction(a: string, b: string): number {
     const splitA = a.split("");
     const splitB = b.split("");
+    const bCharsCount = {};
     let count = 0;
 
-    
+    for (let char of splitB) {
+        bCharsCount.hasOwnProperty(char) ? bCharsCount[char]++ : bCharsCount[char] = 1;
+    }
+
+    while (!Object.values(bCharsCount).includes(0)) {
+        for (let char of splitA) {
+            if (bCharsCount.hasOwnProperty(char)) {
+                bCharsCount[char]--;
+            }
+        }
+        count++;
+    }
 
     return count;
 }
