4e95b93b553b26f8c95488e572003d058e230930
Edmond Luu <eluu472@gmail.com>
2022-12-22 12:00:00 -0800

    sumOddFibonacciNumbers

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,7 +1,19 @@
 export function sumOddFibonacciNums(num: number): number {
-
+    const fibonacciNums = [1, 1];
+    let oddTotal = 0;
+    while (fibonacciNums[fibonacciNums.length - 1] < num) {
+        const lastIndex = fibonacciNums.length;
+        const nextNumber = fibonacciNums[lastIndex - 1] + fibonacciNums[lastIndex - 2];
+        fibonacciNums.push(nextNumber);
+    }
+    for (let element of fibonacciNums) {
+        if (element < num && element % 2 !== 0) {
+            oddTotal += element;
+        }
+    }
+    return oddTotal
 }
 
-// console.log(sumOddFibonacciNums(1000));
-// console.log(sumOddFibonacciNums(4000000));
-// console.log(sumOddFibonacciNums(10));
+console.log(sumOddFibonacciNums(1000));
+console.log(sumOddFibonacciNums(4000000));
+console.log(sumOddFibonacciNums(10));
