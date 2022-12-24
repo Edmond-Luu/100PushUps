e48e6c3889df2e03699b0dee6318bc8523991b3a
Edmond Luu <eluu472@gmail.com>
2022-12-23 16:06:50 -0800

    sumOfTwo

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,12 @@
 export function sumOfTwo(a: number[], b: number[], v: number): boolean {
-
+    for(let aNum of a){
+        for (let bNum of b){
+            if(aNum + bNum === v){
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-// console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 43));
