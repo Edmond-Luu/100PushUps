425cd7839373fdeaba3a27422b237fa70223015a
Edmond Luu <eluu472@gmail.com>
2022-12-23 16:11:00 -0800

    sumOfTwo

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,12 +1,26 @@
 export function sumOfTwo(a: number[], b: number[], v: number): boolean {
-    for(let aNum of a){
-        for (let bNum of b){
-            if(aNum + bNum === v){
-                return true;
-            }
+    // for(let aNum of a){
+    //     for (let bNum of b){
+    //         if(aNum + bNum === v){
+    //             return true;
+    //         }
+    //     }
+    // }
+    // return false;
+
+
+
+    const hashMap = {};
+    for (let num of a) {
+        const difference = v - num;
+        hashMap[difference] = difference;
+    }
+    for (let num of b) {
+        if(hashMap.hasOwnProperty(num)){
+            return true;
         }
     }
     return false;
 }
 
-console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 43));
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
