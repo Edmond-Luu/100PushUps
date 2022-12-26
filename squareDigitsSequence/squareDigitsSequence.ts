d0e14cb983a7f48990a4f0ec8f0347557217c267
Edmond Luu <eluu472@gmail.com>
2022-12-25 16:38:07 -0800

    squareDigitsSequence

--- squareDigitsSequence/squareDigitsSequence.ts
+++ squareDigitsSequence/squareDigitsSequence.ts
@@ -1,22 +1,23 @@
 export function squareDigitsSequence(a0: number): number {
-    let repeat = true;
     const collection = [a0];
+    let count = 1;
     function splitSquare(num) {
         let total = 0;
         const digits = num.toString().split("");
         digits.forEach(digit => total += parseInt(digit) ** 2);
         return total;
     };
-    while (repeat) {
+    while (true) {
         const newNum = splitSquare(collection[collection.length - 1])
         if (!collection.includes(newNum)) {
             collection.push(newNum);
+            count++;
         } else {
-            return collection.length + 1;
+            count++;
+            return count;
         };
     };
 };
-
 
 console.log(squareDigitsSequence(16));
 console.log(squareDigitsSequence(103));
