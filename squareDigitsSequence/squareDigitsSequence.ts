023cab0c752d35e7231f94e39fe0203951fb5a6c
Edmond Luu <eluu472@gmail.com>
2022-12-25 16:35:56 -0800

    squareDigitsSequence

--- squareDigitsSequence/squareDigitsSequence.ts
+++ squareDigitsSequence/squareDigitsSequence.ts
@@ -1,5 +1,20 @@
 export function squareDigitsSequence(a0: number): number {
-
+    let repeat = true;
+    const collection = [a0];
+    function splitSquare(num) {
+        let total = 0;
+        const digits = num.toString().split("");
+        digits.forEach(digit => total += parseInt(digit) ** 2);
+        return total;
+    };
+    while (repeat) {
+        const newNum = splitSquare(collection[collection.length - 1])
+        if (!collection.includes(newNum)) {
+            collection.push(newNum);
+        } else {
+            return collection.length + 1;
+        };
+    };
 }
 
 
