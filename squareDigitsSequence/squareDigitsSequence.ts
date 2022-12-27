ff2bb882dc63392fc7b778c0f81bfb20481c75e1
Edmond Luu <eluu472@gmail.com>
2022-12-26 16:42:22 -0800

    squareDigitsSequence

--- squareDigitsSequence/squareDigitsSequence.ts
+++ squareDigitsSequence/squareDigitsSequence.ts
@@ -1,22 +1,42 @@
 export function squareDigitsSequence(a0: number): number {
-    const collection = [a0];
+    // const collection = [a0];
+    // let count = 1;
+    // function splitSquare(num) {
+    //     let total = 0;
+    //     const digits = num.toString().split("");
+    //     digits.forEach(digit => total += parseInt(digit) ** 2);
+    //     return total;
+    // };
+    // while (true) {
+    //     count++;
+    //     const newNum = splitSquare(collection[collection.length - 1])
+    //     if (!collection.includes(newNum)) {
+    //         collection.push(newNum);
+    //     } else {
+    //         return count;
+    //     };
+    // };
+
+
+
     let count = 1;
-    function splitSquare(num) {
-        let total = 0;
-        const digits = num.toString().split("");
-        digits.forEach(digit => total += parseInt(digit) ** 2);
-        return total;
+    const uniqueNums = [a0];
+    function digitPower(num: number): number {
+        const digits = num.toString().split("").map(element => parseInt(element) ** 2);
+        const sum = digits.reduce((a, b) => a + b)
+
+        return sum;
     };
     while (true) {
         count++;
-        const newNum = splitSquare(collection[collection.length - 1])
-        if (!collection.includes(newNum)) {
-            collection.push(newNum);
-        } else {
+        a0 = digitPower(a0);
+
+        if (uniqueNums.includes(a0)) {
             return count;
+        } else {
+            uniqueNums.push(a0);
         };
     };
-
 };
 
 console.log(squareDigitsSequence(16));
