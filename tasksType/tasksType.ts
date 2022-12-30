46694ac777caf092b67b2d3995e9289696a1ff39
Edmond Luu <eluu472@gmail.com>
2022-12-29 16:53:08 -0800

    taskTypes

--- tasksType/tasksType.ts
+++ tasksType/tasksType.ts
@@ -1,6 +1,10 @@
 export function tasksTypes(deadlines: number[], day: number): number[] {
-
+    const counts = [];
+    let todayCount = 0;
+    let upcomingCount = 0;
+    let laterCount = 0;
+    return counts;
 }
 
-// console.log(tasksTypes([1, 2, 3, 4, 5], 2));
-// console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
+console.log(tasksTypes([1, 2, 3, 4, 5], 2));
+console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
