e977fedf0f55fc35b59990ace9fae5d2e1a55478
Edmond Luu <eluu472@gmail.com>
2023-01-03 17:01:10 -0800

    taskType

--- tasksType/tasksType.ts
+++ tasksType/tasksType.ts
@@ -23,7 +23,7 @@ export function tasksTypes(deadlines: number[], day: number): number[] {
         }
     });
     return [today, upcoming, later];
-}
+};
 
 console.log(tasksTypes([1, 2, 3, 4, 5], 2));
 console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
