776070fa789a11ebb393586bdf1f5d8de7f181e9
Edmond Luu <eluu472@gmail.com>
2023-01-04 17:00:22 -0800

    uniqueDigitsProducts

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,13 @@
 export function uniqueDigitProducts(a: number[]): number {
+    const products = [];
+    a.forEach(num => {
+        let product = 1;
+        const digits = num.toString().split("").map(digit => parseInt(digit));
+        digits.forEach(value => product *= value);
+        products.push(product);
+    });
+    const uniqueProducts = new Set(products)
+    return [...uniqueProducts].length;
 }
 
-// console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
+console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
