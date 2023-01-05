b3dc5ad75e317755bd999fdeaafdbb88ec78c9e9
Edmond Luu <eluu472@gmail.com>
2023-01-05 10:55:49 -0800

    Added challenges

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,11 @@
 export function validTime(time: string): boolean {
-
+    const [hour, min] = time.split(":");
+    if (parseInt(hour) >= 24 || parseInt(min) >= 60) {
+        return false;
+    };
+    return hour;
 }
 
-// console.log(validTime('13:58'));
-// console.log(validTime('25:51'));
-// console.log(validTime('02:76'));
+console.log(validTime('13:58'));
+console.log(validTime('25:51'));
+console.log(validTime('02:76'));
