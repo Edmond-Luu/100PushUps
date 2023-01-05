015c9ecd0066485a0ca05b3ce866d015f378e4b2
Edmond Luu <eluu472@gmail.com>
2023-01-05 15:50:38 -0800

    validTime

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,9 +1,20 @@
 export function validTime(time: string): boolean {
-    const [hour, min] = time.split(":");
-    if (parseInt(hour) >= 24 || parseInt(min) >= 60) {
+    // const [hour, min] = time.split(":");
+    // if (parseInt(hour) < 0 || parseInt(hour) > 23 || parseInt(min) < 0 || parseInt(min) > 59) {
+    //     return false;
+    // };
+    // return true;
+
+
+    const [hours, minutes] = time.split(":");
+    if (parseInt(hours) < 0 || parseInt(hours) > 23) {
+        return false;
+    };
+    if (parseInt(minutes) < 0 || parseInt(minutes) > 59) {
         return false;
     };
     return true;
+
 };
 
 console.log(validTime('13:58'));
