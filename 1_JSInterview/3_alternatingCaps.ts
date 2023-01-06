e1496637bc4fa1fa377f28a99b56ed5428940f95
Edmond Luu <eluu472@gmail.com>
2023-01-05 16:19:28 -0800

    alternatingCaps

--- 1_JSInterview/3_alternatingCaps.ts
+++ 1_JSInterview/3_alternatingCaps.ts
@@ -6,8 +6,14 @@ Example input: "I'm so happy it's Monday"
 Example output: "I'M So hApPy iT'S MoNdAy"
 */
 
-function altCaps(str){
-    return newStr;
-}
+function altCaps(str) {
+    const chars: string[] = str.split("");
+    const newChars = [];
+    chars.forEach(char => {
+        chars.indexOf(char) % 2 === 0 ? newChars.push(char.toUpperCase()) : newChars.push(char.toLowerCase());
+    })
+    return newChars.join("");
+};
 
 console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
+console.log(altCaps("I'm so happy it's Monday"));
