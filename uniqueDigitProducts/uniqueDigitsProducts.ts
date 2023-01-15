322dc6af98658094a8bef025deef52033b263cd1
Edmond Luu <eluu472@gmail.com>
2023-01-14 17:05:46 -0800

    uniqueDigitsProducts

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,14 +1,28 @@
 export function uniqueDigitProducts(a: number[]): number {
-    const products = [];
-    a.forEach(num => {
+    // const products = [];
+    // a.forEach(num => {
+    //     let product = 1;
+    //     const digits = num.toString().split("").map(digit => parseInt(digit));
+    //     digits.forEach(value => product *= value);
+    //     if (!products.includes(product)) {
+    //         products.push(product);
+    //     }
+    // });
+    // return products.length;
+
+
+
+    function getDigitProduct(num: number) {
+        const digits: string[] = num.toString().split("");
         let product = 1;
-        const digits = num.toString().split("").map(digit => parseInt(digit));
-        digits.forEach(value => product *= value);
-        if (!products.includes(product)) {
-            products.push(product);
-        }
+        digits.forEach(digit => product *= parseInt(digit));
+        return product;
+    }
+
+    const products: number[] = [];
+    a.forEach(num => {
+        const product = getDigitProduct(num);
     });
-    return products.length;
 };
 
 console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
