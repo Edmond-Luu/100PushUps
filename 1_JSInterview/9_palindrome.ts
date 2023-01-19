7dd88db7ce87fe8d50a24d2bbd99e6cc51534e2e
Edmond Luu <eluu472@gmail.com>
2023-01-19 14:59:14 -0800

    palindrome

--- 1_JSInterview/9_palindrome.ts
+++ 1_JSInterview/9_palindrome.ts
@@ -18,6 +18,11 @@ function isPalindrome(str) {
     for (let i = splitStr.length - 1; i >= 0; i--) {
         reversedStr.push(splitStr[i]);
     };
+    for(let i = 0; i <= splitStr.length; i++){
+        if(splitStr[i] !== reversedStr[i]){
+            return false;
+        }
+    }
 };
 
 // Test your function
